3a5e3a9666969d12eb6a799fd92c453f5efedd1e
joao-oliveira-techbiosis <joao.oliveira@techbiosis.com>
2019-11-20 15:10:05 +0000

    Cache

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor-gatsby\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor-gatsby\\src\\pages\\404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor-gatsby\\src\\pages\\index.js"))),
-  "component---src-pages-library-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor-gatsby\\src\\pages\\library.js"))),
-  "component---src-pages-page-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor-gatsby\\src\\pages\\page.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor\\src\\pages\\index.js"))),
+  "component---src-pages-library-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor\\src\\pages\\library.js"))),
+  "component---src-pages-page-js": hot(preferDefault(require("C:\\Users\\vitor\\Documents\\Lektor\\lektor\\src\\pages\\page.js")))
 }
 
